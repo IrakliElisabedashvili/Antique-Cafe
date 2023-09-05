e48f776f03c882f3f13c1f46437cad87f2b5c5a2
IrakliElisabedashvili <elisabedashvili.irakli@gmail.com>
2023-09-06 00:14:07 +0400

    Website Template

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// Scrollspy Styles
+let nav = document.querySelector('nav');
+let scrollButton = document.getElementById('scrolltop');
+window.onscroll = () => {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        nav.style.padding = '0px 0px';
+        nav.style.backgroundColor = 'black';
+        nav.style.opacity = '0.7';
+        scrollButton.style.display = 'flex';
+    } else if (document.body.scrollTop < 10 || document.documentElement.scrollTop < 10) {
+        nav.style.padding = '10px 0 3px';
+        nav.style.backgroundColor = 'transparent';
+        scrollButton.style.display = 'none';
+    }
+}
+
+// Scrollspy Styles End
+
+
+// Navbar Toggler Button Styles
+
+let navToggler = document.getElementById('navtoggler');
+
+navToggler.addEventListener('click', (el) => {
+    navToggler.classList.toggle('activate');
+})
+
+
+// Navbar Toggler Button Styles End
+
+
+// Smooth Scroll Start
+
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+// Smooth Scroll End
